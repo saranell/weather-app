ba8ceee65359763ff8e964d68cfd9794c5846042
saranell <76405224+saranell@users.noreply.github.com>
2021-02-28 15:20:28 -0600

    Changed js icon link for any city

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function displayWeatherCondition(response) {
   document.querySelector("#sunset").innerHTML = response.data.sys.sunset;
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/10d@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
 
