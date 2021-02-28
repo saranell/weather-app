2052e2372cadc8d89081361285c2b8765fe91218
saranell <76405224+saranell@users.noreply.github.com>
2021-02-28 15:31:50 -0600

    Alt text for icon updated to current api

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ function displayWeatherCondition(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
