776826fe68780cf52d2ede7e9213d187658ab7f9
saranell <76405224+saranell@users.noreply.github.com>
2021-03-06 15:11:00 -0600

    Formatting forecast html & js

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
-let dateElement = document.querySelector("#date");
-let currentTime = new Date();
-let hours = currentTime.getHours();
-let minutes = currentTime.getMinutes();
-let dayIndex = currentTime.getDay();
-let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-let humidity = document.querySelector("#humidity");
-let iconElement = document.querySelector("#icon");
-let tempMax = document.querySelector("#temp_max");
-let tempMin = document.querySelector("#temp_min");
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
 
-dateElement.innerHTML = `${days[dayIndex]} ${hours}:${minutes}`;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
+}
+
+function formatHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
 
 function displayWeatherCondition(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
@@ -25,7 +49,7 @@ function displayWeatherCondition(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-  let descriptionElement = document.querySelector("#description");
+
   descriptionElement.innerHTML = response.data.weather[0].description;
   document.querySelector("temp_max").innerHTML = Math.round(
     response.data.main.temp_max
@@ -34,11 +58,40 @@ function displayWeatherCondition(response) {
     response.data.main.temp_min
   );
 }
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
 
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+
+    forecastElement.innerHTML += `
+  <div class="col-2">
+    <h3>
+        ${formatHours(forecast.dt * 1000)}
+    </h3>
+    <img
+        src="http://openweathermap.org/img/wn/${
+          forecast.weather[0].icon
+        }@2x.png"
+    />
+    <div class="weather-forecast-temperature">
+        <strong>${Math.round(forecast.main.temp_max)}°</strong> ${Math.round(
+      forecast.main.temp_min
+    )}°
+    </div>
+    </div>
+    `;
+  }
+}
 function search(city) {
   let apiKey = "2c65ee63631088d8b35830f963cef245";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayWeatherCondition);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
