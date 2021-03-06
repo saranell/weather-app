adef6f4acbf8193edd55b9d9f2e1e70b6c2b58ea
saranell <76405224+saranell@users.noreply.github.com>
2021-03-06 16:48:44 -0600

    Added timestamp

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ function displayWeatherCondition(response) {
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
   document.querySelector("#city").innerHTML = response.data.name;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
